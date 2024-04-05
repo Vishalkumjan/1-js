51bb685d50d8525cdcb5ff9ef06255beffb1400d
Vishalkumjan <165926778+Vishalkumjan@users.noreply.github.com>
2024-04-05 16:54:52 +0000

    datatypes

---
+++ basic/1variables.js
@@ -0,0 +1,11 @@
+const accountId =98978
+let accountEmail = "vishal@google.com"
+var accountPasword ="12345"
+ accountCity = "Purnia"
+let accountState;
+
+//  accountId=2 // not allowed
+
+ console.log(accountId)
+
+ console.table([accountEmail , accountId , accountPasword , accountCity , accountState])
---
+++ basic/2v.js
@@ -0,0 +1,18 @@
+"use strict" // treat all js code as new version//
+
+console.log(3+3) 
+ console.log("vishal jha ")
+
+ let name = "vishal"
+ let age= 19
+ let IsloggedIn = false
+
+ // number => 2 to power 53
+ // bigint
+ // string ""
+  //boolean => True/ false
+  //null => standalone value
+  // undefined
+  // symbol => unique
+
+  console.log(typeof null);
