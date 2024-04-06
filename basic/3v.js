ff471ff8bc8791c061419bf9434da6a1bab1d0e7
Vishalkumjan <165926778+Vishalkumjan@users.noreply.github.com>
2024-04-06 15:19:20 +0000

    conversion

---
+++ basic/3v.js
@@ -0,0 +1,13 @@
+let score="False"
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber );
+console.log( valueInNumber);
+
+
+let isLoggedIn = 1
+let booleanIsloggedIn = Boolean(isLoggedIn)
+console.log(booleanIsloggedIn )
+
