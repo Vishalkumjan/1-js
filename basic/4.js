825f82ee30d3937b10e28729a4b75cee257540df
Vishalkumjan <165926778+Vishalkumjan@users.noreply.github.com>
2024-04-07 04:14:27 +0000

    operations

---
+++ basic/4.js
@@ -0,0 +1,24 @@
+// operations //
+let value =3
+let negVlaue = -value
+console.log(negVlaue);
+
+let str1="hello"
+
+let str= " Vishal"
+
+let str3= str1+str 
+console.log(str3);
+
+console.log("1" +"8");
+console.log(  1+1+"2");
+
+console.log(+true);
+console.log(+"");
+
+let num1 , num2 , num3
+num1=num2=num3=2+2
+
+let gameConter=100
+++gameConter;
+console.log(gameConter);
