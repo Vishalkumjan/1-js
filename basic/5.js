74a9473bb9086c0b9c1438654524d995c011c333
Vishalkumjan <165926778+Vishalkumjan@users.noreply.github.com>
2024-04-09 17:24:08 +0000

    datatypes1

---
+++ basic/5.js
@@ -0,0 +1,16 @@
+
+//DataTypes
+
+
+// primitive
+// 7 types : string , number , boolean , null ,  undefine , bigInt , symbol 
+
+
+
+// Reference( non primitive )
+
+// Array , object , Function
+
+const Id = Symbol("234")
+const anotherId = Symbol("234")
+console.log(Id===anotherId);
